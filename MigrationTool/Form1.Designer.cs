5eb4331dece6c145f606a1c9b03caf09940fb3e7
Harshald2019 <hiharsh.harshal@gmail.com>
2019-03-29 13:08:10 +0530

    code checin

---
+++ MigrationTool/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace MigrationTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtSearchBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupByGrid1 = new MigrationTool.GroupByGrid();
+            this.panel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupByGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Solution File";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(537, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.textBox1);
+            this.panel.Controls.Add(this.btnBrowse);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(800, 49);
+            this.panel.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.txtSearchBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 35);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(631, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(210, 35);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtSearchBox
+            // 
+            this.txtSearchBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtSearchBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.txtSearchBox.Location = new System.Drawing.Point(330, 8);
+            this.txtSearchBox.Multiline = true;
+            this.txtSearchBox.Name = "txtSearchBox";
+            this.txtSearchBox.Size = new System.Drawing.Size(295, 20);
+            this.txtSearchBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Search Text :";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Indent = 20;
+            this.treeView1.Location = new System.Drawing.Point(0, 84);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(210, 366);
+            this.treeView1.TabIndex = 5;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect_1);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(210, 84);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(590, 366);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupByGrid1
+            // 
+            this.groupByGrid1.AllowUserToAddRows = false;
+            this.groupByGrid1.AllowUserToDeleteRows = false;
+            this.groupByGrid1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.groupByGrid1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.groupByGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.groupByGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupByGrid1.Location = new System.Drawing.Point(210, 84);
+            this.groupByGrid1.Name = "groupByGrid1";
+            this.groupByGrid1.ReadOnly = true;
+            this.groupByGrid1.Size = new System.Drawing.Size(590, 366);
+            this.groupByGrid1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupByGrid1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Solution Analyzer Tool";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupByGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtSearchBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnSearch;
+        private GroupByGrid groupByGrid1;
+    }
+}
+
---
+++ MigrationTool/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+using Microsoft.Build.BuildEngine;
+using Microsoft.Build.Construction;
+
+//using Project = Microsoft.Build.Evaluation.Project;
+
+namespace MigrationTool
+{
+    public partial class Form1 : Form
+    {
+        private TreeNode _root = new TreeNode("Solution");
+        private TreeNode _filenode = new TreeNode("Files");
+        string _path = "";
+        private static Solution _solutionFile = null;
+        private DirectoryInfo _parentDirectory = null;
+        private List<string> ext = new List<string> { ".cs", ".ts", ".config", " *.jpg", "*.gif", "*.cpp", "*.c", "*.htm", "*.html", "*.xsp", "*.asp", "*.xml", "*.h", "*.asmx", "*.asp", "*.atp", "*.bmp", "*.dib", "*.config", "*.sln", "*.txt" };
+        static DataTable _projects = null;
+        readonly char[] splitchar = new char[] { ' ', ',' };
+        readonly string _filepath = Path.GetDirectoryName(Application.ExecutablePath) + "\\Autocomplete.txt";
+        AutoCompleteStringCollection _autocompleteList = new AutoCompleteStringCollection();
+        private GroupByGrid groupByGrid = null;
+        public Form1()
+        {
+            InitializeComponent();
+            groupByGrid = new GroupByGrid();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.LabelEdit = true;
+            listView1.FullRowSelect = true;
+            listView1.Sorting = SortOrder.Ascending;
+            ApplyAutoFilter();
+        }
+
+        private void ApplyAutoFilter()
+        {
+            if (!File.Exists(_filepath))
+            {
+                File.Create(_filepath);
+            }
+            else
+            {
+                using (var reader = new StreamReader(_filepath))
+                {
+                    while (!reader.EndOfStream)
+                        _autocompleteList.Add(reader.ReadLine());
+                }
+
+                txtSearchBox.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                txtSearchBox.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                txtSearchBox.AutoCompleteCustomSource = _autocompleteList;
+            }
+        }
+
+
+        private void GetProjects()
+        {
+            openFileDialog1.Filter = @"solution files (*.sln)|*.sln";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                textBox1.Text = file;
+                try
+                {
+                    _parentDirectory = Directory.GetParent(file);
+                    _solutionFile = new Solution(file);
+                    treeView1.Nodes.Add(_root);
+
+                    if (_solutionFile.Projects == null) return;
+                    var solutionDir = Path.GetDirectoryName(file);
+
+                    var prjFiles = Directory.GetFiles(solutionDir ?? throw new InvalidOperationException(), "*.*", SearchOption.AllDirectories)
+                        .Where(s => s.EndsWith(".csproj"));
+                    foreach (var prjFile in prjFiles)
+                    {
+                        //Engine.GlobalEngine.BinPath = @"C:\Windows\Microsoft.NET\Framework\v2.0.xxxxx";
+
+                        // Create a new empty project
+                        var project = new Project();
+
+                        // Load a project
+                        project.Load(prjFile);
+
+                        if (project.PropertyGroups == null) continue;
+                        foreach (BuildPropertyGroup propertyGroup in project.PropertyGroups)
+                        {
+                            foreach (BuildProperty prop in propertyGroup)
+                            {
+                                MessageBox.Show(string.Format("{0}:{1}", prop.Name, prop.Value));
+                            }
+                        }
+
+
+                        //XmlDocument xmldoc = new XmlDocument();
+                        //xmldoc.Load(prjFile);
+                        //XmlNamespaceManager ns = new XmlNamespaceManager(xmldoc.NameTable);
+                        //ns.AddNamespace("msbld", "http://schemas.microsoft.com/developer/msbuild/2003");
+                        //XmlNode node = xmldoc.SelectSingleNode("/Project/PropertyGroup/AssemblyName", ns);
+
+                        //if (node != null)
+                        //{
+                        //    MessageBox.Show(node.InnerText);
+                        //}
+                    }
+
+
+                    foreach (var d in _solutionFile.Projects)
+                    {
+                        var projNode = new TreeNode(d.ProjectName);
+                        _root.Nodes.Add(projNode);
+                    }
+                }
+                catch (IOException ex)
+                {
+                }
+            }
+        }
+
+        //Data to be dispaly on the left panel
+        //private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        //{
+        //    for (int i = 0; i < listView1.Items.Count; i++)
+        //    {
+        //        if (listView1.Items[i].Selected == true)
+        //        {
+        //            _path = listView1.Items[i].Name;
+        //            textBox1.Text = _path;
+        //            listView1.Items.Clear();
+        //            //LoadFilesAndDir(_path);
+        //        }
+        //    }
+        //}
+
+        /// <summary>
+        /// Browse the solution file to explore its content.
+        /// </summary>
+        /// <param name="sender">button object</param>
+        /// <param name="e">button event argument</param>
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            GetProjects();
+        }
+
+        /// <summary>
+        /// Create default datatable header content.
+        /// </summary>
+        private static void CreateTemplateTable()
+        {
+            _projects = new DataTable();
+            // add column to datatable  
+            _projects.Columns.Add("Project Name", typeof(string));
+            _projects.Columns.Add("Search Keaywords", typeof(string));
+            _projects.Columns.Add("File Name", typeof(string));
+            _projects.Columns.Add("Line No.", typeof(int));
+            _projects.Columns.Add("Line", typeof(string));
+        }
+
+        private static void GetProjectOutPutType()
+        {
+            //DTE DTE = Package.GetGlobalService(typeof(EnvDTE.DTE)) as DTE;
+            //var project = ((Array) DTE.ActiveSolutionProjects).GetValue(0) as Project;
+            //var properties = project.Properties;
+            //var ot = properties.Item("OutputType").Value.ToString();s
+            //prjOutputType po = (prjOutputType) Enum.Parse(typeof(prjOutputType), ot);
+        }
+
+        private void treeView1_AfterSelect_1(object sender, TreeViewEventArgs e)
+        {
+            try
+            {
+                listView1.Items.Clear();
+                var selectednode = e.Node;
+                treeView1.SelectedNode.ImageIndex = e.Node.ImageIndex;
+                selectednode.Expand();
+                //txtSearchBox.Text = selectednode.FullPath;
+
+                if (selectednode.Nodes.Count > 0)
+                {
+                    foreach (TreeNode n in selectednode.Nodes)
+                    {
+
+                        var lst = new ListViewItem(n.Text, n.ImageIndex);
+                        lst.Name = n.FullPath.Substring(13);
+                        listView1.Items.Add(lst);
+                    }
+                }
+                else
+                {
+                    listView1.Items.Add(selectednode.FullPath, selectednode.Text, selectednode.ImageIndex);
+                }
+            }
+            catch (Exception e1)
+            {
+            }
+
+        }
+
+        private static void ShowMethods(Type type)
+        {
+            foreach (var method in type.GetMethods())
+            {
+                var parameters = method.GetParameters();
+                var parameterDescriptions = string.Join
+                (", ", method.GetParameters()
+                    .Select(x => x.ParameterType + " " + x.Name)
+                    .ToArray());
+
+                Console.WriteLine("{0} {1} ({2})",
+                    method.ReturnType,
+                    method.Name,
+                    parameterDescriptions);
+            }
+        }
+
+        private static void SearchText(string projectName, string filePath, string[] searchTextArray)
+        {
+            var counter = 0;
+            string line;
+
+            // Read the file and display it line by line.
+            var file = new StreamReader(filePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                //if (line.Contains(textToBeSearch))
+                foreach (var textToBeSearch in searchTextArray)
+                {
+                    if (Regex.IsMatch(line, @"\b" + textToBeSearch + @"\b"))
+                        _projects.Rows.Add(projectName, textToBeSearch, Path.GetFileName(filePath), counter, line.Trim());
+
+                }
+
+                counter++;
+            }
+            file.Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            EnableDisableButtons(false);
+            // string searchText = txtSearchBox.Text;
+
+            if (string.IsNullOrEmpty(txtSearchBox.Text))
+            {
+                MessageBox.Show(@"Please enter text that need to search !");
+            }
+            else
+            {
+                var searchTextArray = txtSearchBox.Text.Split(splitchar);
+                TextWriter txt = new StreamWriter(_filepath);
+                txt.Write(searchTextArray.ToList());
+                txt.Close();
+                // File.WriteAllLines(_filepath, searchTextArray.ToList());
+                ApplyAutoFilter();
+
+                if (_solutionFile.Projects.Count > 0)
+                {
+                    CreateTemplateTable();
+
+                    foreach (var projDetails in _solutionFile.Projects)
+                    {
+                        var projDirectory = Path.Combine(_parentDirectory.FullName, projDetails.RelativePath);
+                        var directoryInfo = Directory.GetParent(projDirectory);
+
+                        getFilesAndDir(projDetails.ProjectName, directoryInfo, searchTextArray);
+                    }
+
+                    if (_projects == null || _projects.Rows.Count <= 0) return;
+
+                    var dsDataset = new DataSet();
+                    dsDataset.Tables.Add(_projects);
+                    //dataGridView1.DataSource = dsDataset.Tables[0];
+                    groupByGrid1.DataSource = dsDataset.Tables[0];
+                }
+                else
+                    MessageBox.Show(@"There are no projects in the solution !");
+            }
+            EnableDisableButtons(true);
+        }
+
+        private void EnableDisableButtons(bool isEnable)
+        {
+            btnSearch.Enabled = isEnable;
+            btnBrowse.Enabled = isEnable;
+            //btnBack.Enabled = isEnable;
+            //btnForward.Enabled = isEnable;
+        }
+
+
+        private void getFilesAndDir(string projectName, DirectoryInfo dirname, string[] searchTextArray)
+        {
+            try
+            {
+                var filesInProject = Directory.GetFiles(dirname.FullName, "*.*", SearchOption.AllDirectories)
+                    .Where(s => ext.Contains(Path.GetExtension(s)));
+
+                foreach (var file in filesInProject)
+                {
+                    SearchText(projectName, file, searchTextArray);
+                }
+
+
+
+                //foreach (var fi in dirname.GetFiles())
+                //{
+                //    if (ext.Contains(Path.GetExtension(fi.FullName)))
+                //        SearchText(projectName, fi.FullName, searchText);
+                //}
+                //try
+                //{
+                //    foreach (var di in dirname.GetDirectories())
+                //    {
+                //        getFilesAndDir(projectName, di, searchText);
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //}
+            }
+            catch (Exception e1)
+            {
+            }
+        }
+
+
+    }
+}
